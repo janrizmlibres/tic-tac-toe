ba05960de2962480f71361c016ebc2b6a4aef1b6
Janriz Libres <janriz.libres@elinnovtech.com>
2024-10-27 13:06:16 +0800

    add two way binding to player name input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" value={name} required />
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleNameChange}
+            required
+          />
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
